7f3fa82f9e6f3d928099752958a8003992cd0928
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-27 20:39:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,9 +7,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort do |a,b|
-    a.length <=> b.length
+  array.sort.length #do |a,b|
+    # a.length <=> b.length
   end
 end
-
-def 
